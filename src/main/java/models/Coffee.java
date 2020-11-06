ba59961a5de464a8738705377cea7961ea5d2e2f
Christian Pirhalla <chrispir@udel.edu>
2020-11-05 19:18:15 -0500

    added Coffee properties

--- src/main/java/models/Coffee.java
+++ src/main/java/models/Coffee.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Coffee {
+    private int size; // in fl oz
+    private int qty;
+    private float price;
+    private String drinkType; //regular, latte, iced
+    private String flavor; // none, vanilla, caramel, chocolate, pumpkin spice
+    private boolean cream;
+    private boolean sugar;
+
 }
