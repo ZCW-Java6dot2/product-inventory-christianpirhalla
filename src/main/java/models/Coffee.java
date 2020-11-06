51678f9591a73021389c8e87a4b73fe3a290784b
Christian Pirhalla <chrispir@udel.edu>
2020-11-05 19:22:12 -0500

    added guitar properties and modified formatting in Coffee class

--- src/main/java/models/Coffee.java
+++ src/main/java/models/Coffee.java
@@ -1,9 +1,11 @@
 package models;
 
 public class Coffee {
-    private int size; // in fl oz
+
     private int qty;
     private float price;
+
+    private int size; // in fl oz
     private String drinkType; //regular, latte, iced
     private String flavor; // none, vanilla, caramel, chocolate, pumpkin spice
     private boolean cream;
--- src/main/java/models/Guitar.java
+++ src/main/java/models/Guitar.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Guitar {
+
+    private int qty;
+    private float price;
+
+    private String brand; //fender, gibson, ibanez, peavey
+    private int numStrings; //6-8
+    private String color; //black, white, red, pink, sunburst
+    private boolean hasActivePickups;
 }
