c409aed3ff9074ef1c45f9f54b3ad9a2c34a66eb
Christian Pirhalla <chrispir@udel.edu>
2020-11-05 20:29:38 -0500

    Guitar tests added

--- src/main/java/models/Guitar.java
+++ src/main/java/models/Guitar.java
@@ -9,4 +9,52 @@ public class Guitar {
     private int numStrings; //6-8
     private String color; //black, white, red, pink, sunburst
     private boolean hasActivePickups;
+
+    public void setQty(int i) {
+        this.qty = i;
+    }
+
+    public int getQty() {
+        return this.qty;
+    }
+
+    public void setPrice(double v) {
+        this.price = v;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setBrand(String brnd) {
+        this.brand = brnd;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public void setNumStrings(int i) {
+        this.numStrings = i;
+    }
+
+    public int getNumStrings() {
+        return this.numStrings;
+    }
+
+    public void setColor(String col) {
+        this.color = col;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setHasActivePickups(boolean b) {
+        this.hasActivePickups = b;
+    }
+
+    public Boolean getHasActivePickups() {
+        return hasActivePickups;
+    }
 }
--- src/test/java/models/GuitarTest.java
+++ src/test/java/models/GuitarTest.java
@@ -1,4 +1,87 @@
 package models;
 
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class GuitarTest {
+
+    @Test
+    public void setQtyTest(){
+        //given
+        int expected = 20;
+
+        //when
+        Guitar testGuitar = new Guitar();
+        testGuitar.setQty(20);
+
+        //then
+        int actual = testGuitar.getQty();
+        Assertions.assertEquals(expected,actual);
+    }
+
+    public void setPriceTest(){
+        //given
+        double expected = 200.00;
+
+        //when
+        Guitar testGuitar = new Guitar();
+        testGuitar.setPrice(200.00);
+
+        //then
+        double actual = testGuitar.getPrice();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public void setBrandTest(){
+        //given
+        String expected = "Fender";
+
+        //when
+        Guitar testGuitar = new Guitar();
+        testGuitar.setBrand("Fender");
+
+        //then
+        String actual = testGuitar.getBrand();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public void setNumStringsTest(){
+        //given
+        int expected = 7;
+
+        //when
+        Guitar testGuitar = new Guitar();
+        testGuitar.setNumStrings(7);
+
+        //then
+        int actual = testGuitar.getNumStrings();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public void setColor() {
+        //given
+        String expected = "black";
+
+        //when
+        Guitar testGuitar = new Guitar();
+        testGuitar.setColor("black");
+
+        //then
+        String actual = testGuitar.getColor();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public void setHasActivePickupsTest() {
+        //given
+        Boolean expected = false;
+
+        //when
+        Guitar testGuitar = new Guitar();
+        testGuitar.setHasActivePickups(false);
+
+        //then
+        Boolean actual = testGuitar.getHasActivePickups();
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
