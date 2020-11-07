0b016258cf21065f8e5b63ffa355ab341abd6e6a
Christian Pirhalla <chrispir@udel.edu>
2020-11-06 21:10:11 -0500

    console added

---
+++ src/main/java/io/Console.java
@@ -0,0 +1,13 @@
+package io;
+
+public class Console {
+    public static void printWelcome(){
+        System.out.println(""+
+                "***************************************************" +
+                "|           Welcome and Bem-vindo                 |" +
+                "|                    to                           |" +
+                "|     The Guitar & Coffee Shop Inventory Manager  |" +
+                "***************************************************" );
+    }
+
+}
